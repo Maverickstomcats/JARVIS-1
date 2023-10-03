4f7a18454cda72f6dd3a1ed077ea5eecc1421c8d
Maverickstomcats <113930372+Maverickstomcats@users.noreply.github.com>
2023-10-03 16:18:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
-print("Hello! I am JARVIS")
+import pyttsx3
+engine = pyttsx3.init()
+engine.say("Hello, this is Jarvis 2.0")
+
+def speak(audio):
+    engine.say(audio)
+    engine.runAndWait()
+
+
+def getvoices()
+while True: 
+    audio = input("enter the text to convert it into speech:  ")
+    speak(audio)
+
